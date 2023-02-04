67f9b7985fe8c24cbf1f54f714e8dd09f7e72de2
Prateek Joshi <prateekjoshi565@gmail.com>
2023-02-04 13:42:24 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,13 @@
+import os
+
 def main():
   print("Hello from github actions!")
+  my_key = os.environ.get("MY_SECRET_KEY")
+  
+  if not my_key:
+    raise RuntimeError("MY_SECRET_KEY env var is not set!")
+  
+  print("All good!")
   
 if __name__ == '__main__':
   main()
