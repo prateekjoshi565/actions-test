dc68d9b5788222893066074c1d22d0dfcae7cf35
Prateek Joshi <prateekjoshi565@gmail.com>
2023-02-04 13:54:16 +0530

    Update example.py

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ def main():
   if not my_key:
     raise RuntimeError("MY_SECRET_KEY env var is not set!")
   
-  print("All good!")
+  print(f"All good! {my_key}")
   
 if __name__ == '__main__':
   main()
